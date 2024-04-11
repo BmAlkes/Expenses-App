5decf9075ae2371f295eeeb785c26defd3732736
bmalkes <bmalkes@gmail.com>
2024-04-11 16:45:24 +0300

    feat:add Context API

---
+++ src/context/BudgetContext.tsx
@@ -0,0 +1,29 @@
+import { useReducer, createContext, Dispatch, ReactNode } from "react";
+import {
+  BudgetActions,
+  BudgetState,
+  budgetReducer,
+  initialState,
+} from "../reducers/budget-reducer";
+
+type BudgetContextProps = {
+  state: BudgetState;
+  dispatch: Dispatch<BudgetActions>;
+};
+
+export const BudgetContext = createContext<BudgetContextProps>(
+  {} as BudgetContextProps
+);
+
+type BudgetProviderProps = {
+  children: ReactNode;
+};
+
+export const BudgetProvider = ({ children }: BudgetProviderProps) => {
+  const [state, dispatch] = useReducer(budgetReducer, initialState);
+  return (
+    <BudgetContext.Provider value={{ state, dispatch }}>
+      {children}
+    </BudgetContext.Provider>
+  );
+};
