2c4fda3acf33d6bdbe1838bbc71237868a3e1233
bmalkes <bmalkes@gmail.com>
2024-04-17 20:59:44 +0300

    feat:add reset button

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,9 +1,11 @@
 import { useMemo } from "react";
 import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplat";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
 
 const BudgetTracker = () => {
-  const { state } = useBudget();
+  const { state, dispatch } = useBudget();
 
   const spend = useMemo(
     () => state.expenses.reduce((acc, item) => (acc += item.amount), 0),
@@ -11,16 +13,27 @@ const BudgetTracker = () => {
   );
 
   const available = useMemo(() => state.budget - spend, [state.expenses]);
+  const percentage = +((spend / state.budget) * 100).toFixed(2);
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
       <div className="flex justify-center">
-        <img src="/grafico.jpg" alt=" graphic expanses" />
+        <CircularProgressbar
+          value={percentage}
+          styles={buildStyles({
+            pathColor: percentage >= 85 ? "#dc2626" : "#3b82f6",
+            trailColor: "#f5f5f5",
+            textSize: 8,
+            textColor: "#3b82f6",
+          })}
+          text={`${percentage}% expenses`}
+        />
       </div>
       <div className="flex flex-col items-center gap-8 justify-center">
         <button
           type="button"
           className="bg-pink-600 w-full p-2 text-white uppercase font-bold rounded-lg"
+          onClick={() => dispatch({ type: "reset-app" })}
         >
           Reset App
         </button>
